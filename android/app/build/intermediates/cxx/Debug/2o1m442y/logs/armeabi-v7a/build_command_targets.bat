bad1fc136f284ce31178322bed10a6eef88583f5
Bazoro18 <bazoro.18@gmail.com>
2025-06-25 17:14:45 +0530

    Restored to Initial Commit

--- android/app/build/intermediates/cxx/Debug/2o1m442y/logs/armeabi-v7a/build_command_targets.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\\Users\\imite\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\AirMeasure\\AirMeasure\\android\\app\\.cxx\\Debug\\2o1m442y\\armeabi-v7a" ^
-  appmodules ^
-  react_codegen_rnscreens ^
-  react_codegen_safeareacontext
